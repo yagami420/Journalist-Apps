e1807f2c630279eac8b288fb73e13a3059f64c97
yagami420 <farhan.yagami@gmail.com>
2026-07-18 12:16:14 +0500

    Delete app.py

--- app.py
+++
@@ -1,5 +0,0 @@
-AttributeError: This app has encountered an error. The original error message is redacted to prevent data leaks. Full error details have been recorded in the logs (if you're on Streamlit Cloud, click on 'Manage app' in the lower right of your app).
-Traceback:
-File "/mount/src/journalist-apps/app.py", line 507, in <module>
-    ticker_text = data.get("tickers", "").strip()
-                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
